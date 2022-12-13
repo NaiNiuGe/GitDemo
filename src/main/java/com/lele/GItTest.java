058ee8421dc50a7a0fe4f86539fc84215ad500d7
lele <lele@qq.com>
2022-12-13 15:53:21 +0800

    First

---
+++ src/main/java/com/lele/GItTest.java
@@ -0,0 +1,13 @@
+package com.lele;
+
+/**
+ * @BelongsProject: GitDemo
+ * @BelongsPackage: com.lele
+ * @Author: lele
+ * @CreateTime: 2022-12-13  15:50
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
