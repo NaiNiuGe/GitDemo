e328c67978a113c2653bc5d845760e23d6b98adb
lele <lele@qq.com>
2022-12-13 16:17:39 +0800

    majorFirst

--- src/main/java/com/lele/GItTest.java
+++ src/main/java/com/lele/GItTest.java
@@ -8,6 +8,10 @@ package com.lele;
  */
 public class GItTest {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld1");
+        System.out.println("helloworld2");
+        System.out.println("helloworld3");
+        System.out.println("helloworld4");
+        System.out.println("major");
     }
 }
