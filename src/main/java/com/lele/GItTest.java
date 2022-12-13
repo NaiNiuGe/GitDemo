cd5b79f5d3fef392ffe4c3550c96ccf0dbe4315e
lele <lele@qq.com>
2022-12-13 15:59:26 +0800

    Second

--- src/main/java/com/lele/GItTest.java
+++ src/main/java/com/lele/GItTest.java
@@ -8,6 +8,7 @@ package com.lele;
  */
 public class GItTest {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld1");
+        System.out.println("helloworld2");
     }
 }
